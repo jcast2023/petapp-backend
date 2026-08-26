5b144d1add841836ec547679183df700ae38cf81
jcastili <jul_ed@hotmail.com>
2026-08-26 10:14:21 -0500

    Fix: Desactivar seguridad por defecto de Spring

--- src/main/java/com/petapp/backend/config/SecurityConfig.java
+++ src/main/java/com/petapp/backend/config/SecurityConfig.java
@@ -3,9 +3,15 @@ package com.petapp.backend.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -57,4 +63,22 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Creamos un usuario en memoria (aunque no lo usarás para login real, ya que usas JWT)
+        // Esto "engañará" a Spring Boot para que NO genere la contraseña aleatoria.
+        UserDetails user = User.withUsername("admin")
+                .password("{noop}admin") // {noop} significa sin encriptar, solo para desactivar el fallback
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 }
